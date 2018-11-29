a64f73a93c68a271037f6c27bf6606104e40bc59
enlove <100108@163.com>
2018-11-29 10:32:31 +0800

    Add files via upload
    translation zh-CN

---
+++ dist/extensions/ext-locale/star/zh-CN.js
@@ -0,0 +1,23 @@
+var svgEditorExtensionLocale_star_zh_CN = (function () {
+  'use strict';
+
+  var zhCN = {
+    name: '星形',
+    buttons: [{
+      title: '星形工具'
+    }],
+    contextTools: [{
+      title: '顶点数',
+      label: '顶点数'
+    }, {
+      title: '钝度',
+      label: '钝度'
+    }, {
+      title: '径向变形',
+      label: '径向'
+    }]
+  };
+
+  return zhCN;
+
+}());
