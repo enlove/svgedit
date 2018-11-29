96c90cd5d4c593cd402eeeb6a15cd37cf9e69018
enlove <100108@163.com>
2018-11-29 10:32:54 +0800

    Add files via upload
    translation zh-CN

---
+++ dist/extensions/ext-locale/storage/zh-CN.js
@@ -0,0 +1,17 @@
+var svgEditorExtensionLocale_storage_zh_CN = (function () {
+  'use strict';
+
+  var zhCN = {
+    message: 'By default and where supported, SVG-Edit can store your editor ' + 'preferences and SVG content locally on your machine so you do not ' + 'need to add these back each time you load SVG-Edit. If, for privacy ' + 'reasons, you do not wish to store this information on your machine, ' + 'you can change away from the default option below.',
+    storagePrefsAndContent: '本地存储配置参数和SVG图',
+    storagePrefsOnly: '本地只存储配置参数',
+    storagePrefs: '本地存储配置参数',
+    storageNoPrefsOrContent: '本地不保存配置参数和SVG图',
+    storageNoPrefs: '本地不保存配置参数',
+    rememberLabel: '记住选择?',
+    rememberTooltip: '如果您勾选记住选择，将不再弹出本窗口.'
+  };
+
+  return zhCN;
+
+}());
