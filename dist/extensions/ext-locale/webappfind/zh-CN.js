471ea6a20bd074f5872d3af0b84bb72739589357
enlove <100108@163.com>
2018-11-29 10:33:20 +0800

    Add files via upload
    translation zh-CN

---
+++ dist/extensions/ext-locale/webappfind/zh-CN.js
@@ -0,0 +1,13 @@
+var svgEditorExtensionLocale_webappfind_zh_CN = (function () {
+  'use strict';
+
+  var zhCN = {
+    name: 'WebAppFind',
+    buttons: [{
+      title: '保存图像到本地'
+    }]
+  };
+
+  return zhCN;
+
+}());
