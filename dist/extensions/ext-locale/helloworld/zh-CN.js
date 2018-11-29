c05a51bd022c77149b17d7f22aca9403c8266a83
enlove <100108@163.com>
2018-11-29 10:28:23 +0800

    Add files via upload
    translation zh-CN

---
+++ dist/extensions/ext-locale/helloworld/zh-CN.js
@@ -0,0 +1,14 @@
+var svgEditorExtensionLocale_helloworld_zh_CN = (function () {
+  'use strict';
+
+  var zhCN = {
+    name: 'Hello World',
+    text: 'Hello World!\n\nYou clicked here: {x}, {y}',
+    buttons: [{
+      title: "Say 'Hello World'"
+    }]
+  };
+
+  return zhCN;
+
+}());
