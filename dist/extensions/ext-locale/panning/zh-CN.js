628cacabc5064c71ef4ebbda5fbb13c087467fc7
enlove <100108@163.com>
2018-11-29 10:30:09 +0800

    Add files via upload
    translation zh-CN

---
+++ dist/extensions/ext-locale/panning/zh-CN.js
@@ -0,0 +1,13 @@
+var svgEditorExtensionLocale_panning_zh_CN = (function () {
+  'use strict';
+
+  var zhCN = {
+    name: '扩展平移',
+    buttons: [{
+      title: '平移'
+    }]
+  };
+
+  return zhCN;
+
+}());
