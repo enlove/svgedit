c59651e5454c69ae8c434325a72c2d72ad99d38f
enlove <100108@163.com>
2018-11-29 10:28:52 +0800

    Add files via upload
    translation zh-CN

--- dist/extensions/ext-locale/imagelib/zh-CN.js
+++ dist/extensions/ext-locale/imagelib/zh-CN.js
@@ -2,28 +2,33 @@ var svgEditorExtensionLocale_imagelib_zh_CN = (function () {
   'use strict';
 
   var zhCN = {
-    select_lib: 'Select an image library',
-    show_list: 'Show library list',
-    import_single: 'Import single',
-    import_multi: 'Import multiple',
-    open: 'Open as new document',
+    select_lib: '选择图形库',
+    show_list: '显示列表',
+    import_single: '导入单个',
+    import_multi: '导入多个',
+    open: '打开为新文档',
     buttons: [{
-      title: '图像库'
+      title: '图形库'
     }],
     imgLibs: [{
-      name: 'Demo library (local)',
+      name: '本地演示库',
       url: '{path}imagelib/index{modularVersion}.html',
       description: 'Demonstration library for SVG-edit on this server'
     }, {
-      name: 'IAN Symbol Libraries',
+      name: 'IAN 符号库',
       url: 'https://ian.umces.edu/symbols/catalog/svgedit/album_chooser.php?svgedit=3',
-      description: 'Free library of illustrations'
+      description: '免费插图库' // The site is no longer using our API, and they have added an
+      //   `X-Frame-Options` header which prevents our usage cross-origin:
+      // Getting messages like this in console:
+      //   Refused to display 'https://openclipart.org/detail/307176/sign-bike' in a frame
+      //   because it set 'X-Frame-Options' to 'sameorigin'.
+      // url: 'https://openclipart.org/svgedit',
+      // However, they do have a custom API which we are using here:
+
       /*
-      // See message in "en" locale for further details
-      ,
       {
         name: 'Openclipart',
-        url: 'https://openclipart.org/svgedit',
+        url: '{path}imagelib/openclipart{modularVersion}.html',
         description: 'Share and Use Images. Over 100,000 Public Domain SVG Images and Growing.'
       }
       */
